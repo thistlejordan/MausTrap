e3bbe4d615aedd9b42d8663245154caea4990bfa
Malthaez <33078891+thistlejordan@users.noreply.github.com>
2026-03-25 01:03:55 -0500

    Add DestructablesComponent and Crates

---
+++ Assets/Scripts/Components/DestructableComponent.cs
@@ -0,0 +1,125 @@
+﻿using Assets.Scripts.Managers;
+using Assets.Scripts.Models;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Components
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public abstract class DestructableComponent : AnimatedObjectComponent
+    {
+        [SerializeField] private int defense;
+        [SerializeField] private int health;
+        [SerializeField] private int healthMax;
+        [SerializeField] private bool invincible;
+        private Rigidbody2D rigidBody;
+
+        public int Defense { get; }
+
+        public int Health { get; protected set; }
+
+        public int HealthMax { get; }
+
+        public bool Invincible { get; protected set; }
+
+        public Rigidbody2D Rigidbody
+        {
+            get
+            {
+                if (this.rigidBody == null)
+                {
+                    var rigidBody = this.GetComponent<Rigidbody2D>();
+
+                    if (rigidBody != null)
+                    {
+                        this.rigidBody = rigidBody;
+                    }
+                    else
+                    {
+                        Debug.LogError($"DestructableComponent on {gameObject.name} is missing a Rigidbody2D component.");
+                    }
+                }
+
+                return this.rigidBody;
+            }
+        }
+
+        public virtual void RestoreHealth(int value) => this.Health += (this.Health + value > this.HealthMax) ? 0 : value;
+
+        private int CalculateTrueDamage(int damage) => (damage - this.Defense > 0) ? damage - this.Defense : 1;
+
+        private void TakeDamage(int trueDamage) => this.Health = (this.Health - trueDamage >= 0) ? this.Health - trueDamage : 0;
+
+        private void CheckForDeath()
+        {
+            if (this.Health <= 0 && !this.Animator.GetBool("dead"))
+            {
+                Debug.Log($"Health: {this.Health}");
+                this.Animator.SetBool("dead", true);
+                this.Die();
+            }
+        }
+
+        public virtual void ReceiveAttack(AttackModel attack)
+        {
+            if (this.Invincible)
+            {
+                return;
+            }
+
+            this.InvincibilityFrames();
+            this.TakeDamage(CalculateTrueDamage(attack.Damage));
+            this.Knockback(attack.KnockbackDirection, attack.KnockbackForce);
+            this.CheckForDeath();
+        }
+
+        private void InvincibilityFrames()
+        {
+            this.Invincible = true;
+            this.StartCoroutine(IInvincibilityFrames());
+        }
+
+        private IEnumerator IInvincibilityFrames()
+        {
+            var time = 0f;
+            var i = 1;
+
+            while (time < GameManager.Instance.IFramesDuration)
+            {
+                this.SpriteRenderer.enabled = i > GameManager.Instance.m_IFrameFlickerRate ? true : false;
+
+                if (i < (11 - GameManager.Instance.m_IFrameFlickerRate) * 2)
+                {
+                    ++i;
+                }
+                else
+                {
+                    i = 1;
+                }
+
+                time += Time.deltaTime;
+                yield return new WaitForEndOfFrame();
+            }
+
+            this.SpriteRenderer.enabled = true;
+            this.Invincible = false;
+        }
+
+        public virtual Coroutine Knockback(Vector2 direction, float force) => this.StartCoroutine(IKnockback(direction, force));
+
+        private IEnumerator IKnockback(Vector2 direction, float force)
+        {
+            this.Rigidbody.linearVelocity = direction * force;
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        private void Die() => this.StartCoroutine(IDie());
+
+        private IEnumerator IDie()
+        {
+            this.Animator.SetBool("dead", true);
+            yield return new WaitForSeconds(this.Animator.GetCurrentAnimatorStateInfo(0).length);
+            Destroy(this.gameObject);
+        }
+    }
+}
